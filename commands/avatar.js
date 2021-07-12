5264313243070bb9073b64293d992534cb66d7f6
Otchi <adamgragnic@gmail.com>
2021-07-12 12:41:47 +0200

    Ajout de la commande ::avatar

---
+++ commands/avatar.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    // Envoie du message
+    run: message => {
+        message.channel.send(new Discord.MessageEmbed()
+            .setTitle('Avatar de @' + message.author.toString())
+            .setImage(message.author.displayAvatarURL)
+            .setFooter('Arsène Rivière', 'https://cdn.discordapp.com/app-icons/864039126005579807/2cd6da4813aa7a85a65c728304555fa6.png?size=256')
+            .setTimestamp()
+        )
+    },
+    // Nom de la commande à appeler
+    name: 'avatar'
+}
